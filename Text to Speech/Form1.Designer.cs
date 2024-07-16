ae4c2fc0ffeef95558953ec8e6c3897f5171018d
Parsa Latifi <Parsa Latifi@DESKTOP-J9PNUIK>
2024-07-16 17:57:44 +0330

    Add project files.

---
+++ Text to Speech/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Text_to_Speech
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Convert_btn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SelectBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("B Nazanin", 15.75F, System.Drawing.FontStyle.Bold);
+            this.textBox1.Location = new System.Drawing.Point(41, 86);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(330, 132);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Convert_btn
+            // 
+            this.Convert_btn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Convert_btn.Font = new System.Drawing.Font("B Nazanin", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.Convert_btn.Location = new System.Drawing.Point(108, 318);
+            this.Convert_btn.Name = "Convert_btn";
+            this.Convert_btn.Size = new System.Drawing.Size(173, 44);
+            this.Convert_btn.TabIndex = 1;
+            this.Convert_btn.Text = "Convert";
+            this.Convert_btn.UseVisualStyleBackColor = true;
+            this.Convert_btn.Click += new System.EventHandler(this.Convert_btn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("B Nazanin", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label1.Location = new System.Drawing.Point(69, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(277, 55);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Text To Voice";
+            // 
+            // SelectBox
+            // 
+            this.SelectBox.Font = new System.Drawing.Font("B Nazanin", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.SelectBox.FormattingEnabled = true;
+            this.SelectBox.Location = new System.Drawing.Point(41, 224);
+            this.SelectBox.Name = "SelectBox";
+            this.SelectBox.Size = new System.Drawing.Size(181, 25);
+            this.SelectBox.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(398, 436);
+            this.Controls.Add(this.SelectBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Convert_btn);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "تبدیل متن به صدا";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Convert_btn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox SelectBox;
+    }
+}
+
---
+++ Text to Speech/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Speech.Synthesis;
+using System.Windows.Forms;
+
+namespace Text_to_Speech
+{
+    public partial class Form1 : Form
+    {
+        private SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+        public Form1()
+        {
+            InitializeComponent();
+            SelectBox.Items.Add("Male");
+            SelectBox.Items.Add("Female");
+            SelectBox.Text = "Please Select a Voice";
+        }
+    
+        private void Convert_btn_Click(object sender, EventArgs e)
+        {
+            
+            if (SelectBox.SelectedItem=="Male")
+            {
+                synthesizer.SelectVoiceByHints(VoiceGender.Male, VoiceAge.Adult);
+                synthesizer.Speak(textBox1.Text);
+            }
+            else if (SelectBox.SelectedItem=="Female")
+            {
+                synthesizer.SelectVoiceByHints(VoiceGender.Female, VoiceAge.Adult);
+                synthesizer.Speak(textBox1.Text);
+            }
+            else
+            {
+                MessageBox.Show("Please Select a Voice", "Error",MessageBoxButtons.OK
+                ,MessageBoxIcon.Error);
+            }
+          
+           
+
+        }
+
+    
+        
+        
+    }
+}
